ce7745dc380e874a956ca6e5d44f4b3ffc6d6793
hazalozturk <hazal.ozturk@troyweb.com>
2021-09-05 21:45:02 -0400

    Add subliminal message

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,16 @@ import React from 'react'
 import logo from '../assets/mark.png';
 import '../styles/Header.css';
 
+const goToHome = () => {
+    alert('Hire this cat!')
+}
+
 function Header() {
     return (
         <header className="App-header">
             {/* TODO: Use a high res cat log0, this is a Figma export */}
-            <img src={logo} alt="Cat Logo" className="app-logo" />
-            <h1>Catopia!</h1>
+            <img src={logo} alt="Cat Logo" className="app-logo" onClick={goToHome} />
+            <h1 onClick={goToHome}>Catopia!</h1>
         </header>
     )
 }
